dc8c348fd228fc9ce10cefdf469c968aaf5f0899
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2023-12-27 02:01:11 +0530

    Added Scroll,Text Animations

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ const changeTheme = () => {
     document.querySelector('.theme-img').src = './assets/dark-mode.png';
     document.body.className = 'dark-mode';
 }
+
+const observer = new IntersectionObserver((e) => {
+    e.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hidden = document.querySelectorAll('.hidden');
+hidden.forEach((element) => observer.observe(element));
+
+$(document).ready(function(){
+    $('a').on('click', function(event) {
+        if (this.hash !== "") {
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - $(window).height()/2 + $(hash).height()/2
+            }, 100);
+        }
+    });
+});
